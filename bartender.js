62fdcc0963e27e4f4b17ddee91199ac65519f785
Nicole Vianzon-Kelley <navkelley@gmail.com>
2016-09-27 15:32:44 -1000

    constructor for ingredients

---
+++ bartender.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    //constructor for ingredients 
+    function ingredients (strong, salty, bitter, sweet, fruit) {
+        this.strong = strong;
+        this.salty = salty;
+        this.bitter = bitter;
+        this.sweet = sweet; 
+        this.fruit = fruit;
+    }
+});
