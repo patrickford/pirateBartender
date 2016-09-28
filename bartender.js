219b1e6ee2ee2ef33650923f7b2b5c1cb962e1de
navkelley <navkelley@gmail.com>
2016-09-27 17:10:52 -1000

    create empty array for order create empty array for user order as well as set values for all options to push to array. this will allow array to be used to create drink order as well as compare against pantry items.

--- bartender.js
+++ bartender.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  
+
   //pantry ingredients 
   var pantry = {
     strong: ["Glum of rum", "slug of whisky", "splash of gin"],
@@ -9,12 +9,30 @@ $(document).ready(function() {
     fruit: ["Slice of orange", "Dash of cassis", "Cherry on top"],
   }
 
+  //empty string for user order
+  orderValue = [];
+
   //constructor for ingredients 
-  function ingredients (strong, salty, bitter, sweet, fruit) {
-    this.strong = strong;
-    this.salty = salty;
-    this.bitter = bitter;
-    this.sweet = sweet; 
-    this.fruit = fruit;
+  var userOrder = function (strong, salty, bitter, sweet, fruit) {
+    this.strong = orderValue[0];
+    this.salty = orderValue[1];
+    this.bitter = orderValue[2];
+    this.sweet = orderValue[3]; 
+    this.fruit = orderValue[4];
   }
+
+  //handle sumbit event 
+  $("#orderOptions").submit(function(e){
+    e.preventDefault();
+    //grab user order values and push to orderValue array 
+    $("select").each(function() {
+    if ($(this).val() == "yes") {
+      orderValue.push(true);
+    } 
+    else {
+      orderValue.push(false);
+    }
+    })
+    console.log(orderValue);
+  })
 });
